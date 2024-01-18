1bcd033841d0a6bc50393a4f88ffa595f06a4ca5
Evgeny Sokolov <e.sokolov@gmail.com>
2024-01-18 15:20:06 +0100

    nord colorscheme tweaks for neovim

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -27,9 +27,9 @@ return {
   {
     "shaunsingh/nord.nvim",
     config = function()
-      vim.g.nord_contrast = true
+      vim.g.nord_contrast = false
       vim.g.nord_borders = true
-      vim.g.nord_disable_background = true
+      vim.g.nord_disable_background = not is_neovide()
       vim.g.nord_italic = true
       vim.g.nord_uniform_diff_background = false
       vim.g.nord_bold = true
